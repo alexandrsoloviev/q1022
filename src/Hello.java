b508c4159b97795d80457d9197cc819ea24a31ff
asalauyou <a.salauyou@arateg.com>
2023-03-28 16:24:10 +0300

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+    int value = 0;
+
+    for(int in = 0; in<10;in++){
+        value+=in;
+        System.out.println(in);
+        System.out.println(value);
+    }
+    }
+}
